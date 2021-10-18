7c8e60ab28dacf187b1a8a585f6d1b46dc476c09
Jean Martins <jeanmrtns4@gmail.com>
2021-10-18 14:00:40 -0300

    Final

--- script.js
+++ script.js
@@ -6,30 +6,93 @@ const green = document.querySelector('.green');
 const yellow = document.querySelector('.yellow');
 const red = document.querySelector('.red');
 const blue = document.querySelector('.blue');
-const buttons = [green, yellow, red, blue];
 
-/**
- * Green = 0
- * Yellow = 1
- * Red = 2
- * Blue = 3
- */
+const shuffleOrder = () => {
+  let randomButton = Math.floor(Math.random() * 4);
+  order[order.length] = randomButton;
 
-buttons.forEach((button) => {
-  button.onclick = () => {
-    button.classList.add('active');
+  for (let i in order) {
+    let elementColor = createColorElement(order[i]);
+    lightColor(elementColor, Number(i) + 1);
+  }
+};
 
-    setTimeout(() => {
-      button.classList.remove('active');
-    }, 500);
-  };
-});
+const lightColor = (element, number) => {
+  number *= 500;
+
+  setTimeout(() => {
+    element.classList.add('active');
+  }, number - 250);
+  setTimeout(() => {
+    element.classList.remove('active');
+  });
+};
+
+const checkOrder = () => {
+  for (let i in userOrder) {
+    if (userOrder[i] != order[i]) {
+      gameOver();
+      break;
+    }
+
+    if (userOrder.length == order.length) {
+      alert(`Pontuação: ${score}\nAvançando para o próximo nível!`);
+      nextLevel();
+    }
+  }
+};
+
+const click = (color) => {
+  userOrder[userOrder.length] = color;
+
+  createColorElement(color).classList.add('active');
+
+  setTimeout(() => {
+    createColorElement(color).classList.remove('active');
+    checkOrder();
+  }, 250);
+};
+
+const createColorElement = (color) => {
+  switch (color) {
+    case 0:
+      return green;
+    case 1:
+      return red;
+    case 2:
+      return yellow;
+    case 3:
+      return blue;
+  }
+};
+
+const nextLevel = () => {
+  score++;
+  shuffleOrder();
+};
 
 // Quando o jogador perder a partida.
 const gameOver = () => {
   alert(
-    `Você perdeu 😢.\n Clique em OK para começar novamente!\nScore final: ${score}`
+    `Pontuação: ${score}!\nVocê perdeu o jogo!\nClique em OK para iniciar um novo jogo`
   );
   order = [];
-  userOrder = [];
+  clickedOrder = [];
+
+  playGame();
 };
+
+//funcao de inicio do jogo
+const playGame = () => {
+  alert('Bem vindo ao Gênesis! Iniciando novo jogo!');
+  score = 0;
+
+  nextLevel();
+};
+
+green.onclick = () => click(0);
+red.onclick = () => click(1);
+yellow.onclick = () => click(2);
+blue.onclick = () => click(3);
+
+playGame();
